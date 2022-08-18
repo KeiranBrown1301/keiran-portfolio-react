7a2ca8971ce8ff7fbafb68bf7e4e9a1b2db93aa6
Keiran Brown <keiranbrown1301@gmail.com>
2022-08-18 20:21:41 +1000

    added styling for icons in sidebar

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -3,6 +3,8 @@ import { Link, NavLink } from 'react-router-dom'
 import './sidebar.scss'
 import LogoS from '../../assets/images/logo-s.png';
 import LogoSubtitle from '../../assets/images/logo_sub.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
 
 function Sidebar() {
   return (
@@ -12,8 +14,14 @@ function Sidebar() {
         <img className='sub-logo' src={LogoSubtitle} alt='sub-logo'/>
       </Link>
       <nav>
-        <NavLink>
-
+        <NavLink exact="true" activeclassname="active" to="/" >
+          <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+        </NavLink>
+        <NavLink exact="true" activeclassname="active" className="about-link" to="/about" >
+          <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+        </NavLink>
+        <NavLink exact="true" activeclassname="active" className="contact-link" to="/" >
+          <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
         </NavLink>
       </nav>
     </div>
