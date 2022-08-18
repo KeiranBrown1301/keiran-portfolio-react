39f86e651416f637c755467c2c449d4f1ead6c2b
Keiran Brown <keiranbrown1301@gmail.com>
2022-08-18 19:24:53 +1000

    added logos to sidebar page

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,11 +1,17 @@
-import './sidebar.scss'
 import React from 'react'
+import { Link } from 'react-router-dom'
+import './sidebar.scss'
+import LogoS from '../../assets/images/logo-s.png';
+import LogoSubtitle from '../../assets/images/logo_sub.png';
 
 function Sidebar() {
   return (
-    <>
-
-    </>
+    <div className='nav-bar'>
+      <Link className='logo' to='/'>
+        <img src={LogoS} alt='logo'/>
+        <img src={LogoSubtitle} alt='logo'/>
+      </Link>
+    </div>
   )
 }
 
