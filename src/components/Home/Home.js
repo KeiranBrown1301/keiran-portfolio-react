57a4d67814548d5deccaac80c7438c69f87cff52
Keiran Brown <keiranbrown1301@gmail.com>
2022-08-24 23:04:26 +1000

    added styling for container in layout scss

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,8 +6,15 @@ function Home() {
   return (
     <div className='container home-page'>
       <div className='text-zone'>
-        <h1>Hi, <br /> I'm</h1>
-        <img src={LogoTitle} alt="the developer" />
+        <h1>
+          Hi, <br /> I'm
+          <img src={LogoTitle} alt="the developer" />
+          lobodan
+          <br />
+          web developer
+        </h1>
+        <h2>Frontend Deeloper</h2>
+        <Link to="/contact" className='flat-button'>CONTACT ME</Link>
       </div>
     </div>
   )
