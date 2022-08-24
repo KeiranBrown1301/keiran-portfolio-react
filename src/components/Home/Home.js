33e4efcafc363376b17ab313a74f24793811d0b8
Keiran Brown <keiranbrown1301@gmail.com>
2022-08-24 22:52:09 +1000

    Added logo to home oage

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import LogoTitle from '../../assets/images/logo-s.png'
 import './home.scss'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className='container home-page'>
+      <div className='text-zone'>
+        <h1>Hi, <br /> I'm</h1>
+        <img src={LogoTitle} alt="the developer" />
+      </div>
+    </div>
   )
 }
 
