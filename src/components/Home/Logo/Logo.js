167a47d58117d7d809e5be91f7772499b2d80dc0
Keiran Brown <keiranbrown1301@gmail.com>
2022-09-21 13:40:09 +0900

    All animations on S are somplete, draws in nice and smooth

--- src/components/Home/Logo/Logo.js
+++ src/components/Home/Logo/Logo.js
@@ -5,9 +5,9 @@ import DrawSVGPlugin from 'gsap-trial/DrawSVGPlugin';
 import gsap from 'gsap-trial';
 
 function Logo() {
-  const bgRef = useRef();
-  const outlineLogoRef = useRef();
-  const solidLogoRef = useRef();
+  const bgRef = useRef()
+  const outlineLogoRef = useRef()
+  const solidLogoRef = useRef()
 
   useEffect(() => {
     gsap.registerPlugin(DrawSVGPlugin)
@@ -15,14 +15,23 @@ function Logo() {
     gsap.timeline()
     .to(bgRef.current, {
       duration: 1,
-      opacity: 1
+      opacity: 1,
     })
     .from(outlineLogoRef.current, {
       drawSVG: 0,
       duration: 20,
     })
 
-
+    gsap.fromTo(
+      solidLogoRef.current, {
+        opacity: 0,
+      },
+      {
+        opacity: 1,
+        delay: 4,
+        duration: 4,
+      }
+    )
   }, [])
 
   return (
