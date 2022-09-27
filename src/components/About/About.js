afd5e663400c932f03e2ad3cbd5ccf70ad901bd3
Keiran Brown <keiranbrown1301@gmail.com>
2022-09-27 15:48:13 +0900

    animated letter imported and array with About Me created" " : "

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import AnimatedLetters from '../AnimatedLetters/AnimatedLetters'
 import './about.scss'
 
 function About() {
@@ -6,7 +7,10 @@ function About() {
     <div className='container about-page'>
       <div className='text-zone'>
         <h1>
-
+          <AnimatedLetters
+            strArray={['A', 'b', 'o', 'u', 't', ' ', 'M', 'e']}
+            idx={15}
+          />
         </h1>
       </div>
     </div>
