acb53920626d9b7a5839348c5f6f3213282d2b68
Keiran Brown <keiranbrown1301@gmail.com>
2022-09-27 15:48:30 +0900

    paragraphs added to about me

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -12,6 +12,9 @@ function About() {
             idx={15}
           />
         </h1>
+        <p>I'm a frontend developer currently living in Tokyo. I absolutely love using my creative side together in combination with logic and my problem solving skills in order to create beautiful looking functional apps.</p>
+        <p>Since moving to Tokyo, I've been working as an English Language Instructor and then before that in Australia as an Operations Associate. I took a leap and enrolled in the Full Stack Web Dev course at Le Wagon and the rest is history!</p>
+        <p>Naturally, most of my time is dedicated to coding but when I have a free moment I love to find new ramen spots that I haven't been to before around Tokyo (Jiro FTW!) as well as a bit of gaming here and there and finally Brazilian Jiu-jitsu to keep healthy!.</p>
       </div>
     </div>
   )
