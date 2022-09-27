ef26d48072635c03a8a5188b82e0d51f80f6a388
Keiran Brown <keiranbrown1301@gmail.com>
2022-09-27 16:14:10 +0900

    state and effect from home page copied and used in the about page for bouncy letters.

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,13 +1,22 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import AnimatedLetters from '../AnimatedLetters/AnimatedLetters'
 import './about.scss'
 
 function About() {
+  const [letterClass, setLetterClass] = useState('text-animate')
+
+  useEffect(() => {
+    setTimeout(() => {
+      return setLetterClass('text-animate-hover')
+    }, 3000)
+  }, [])
+
   return (
     <div className='container about-page'>
       <div className='text-zone'>
         <h1>
           <AnimatedLetters
+            letterClass={letterClass}
             strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']}
             idx={15}
           />
