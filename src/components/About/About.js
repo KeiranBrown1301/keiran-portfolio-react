446ba5f7ded412b6ea0922a22a6af2446c04c451
Keiran Brown <keiranbrown1301@gmail.com>
2022-09-27 16:26:28 +0900

    icons added for animated cube

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,6 +1,9 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCss3, faGitAlt, faHtml5, faJsSquare, faReact } from "@fortawesome/free-brands-svg-icons";
 import React, { useState, useEffect } from 'react'
 import AnimatedLetters from '../AnimatedLetters/AnimatedLetters'
 import './about.scss'
+import { faSmile } from '@fortawesome/free-solid-svg-icons';
 
 function About() {
   const [letterClass, setLetterClass] = useState('text-animate')
@@ -24,6 +27,47 @@ function About() {
         <p>I'm a frontend developer currently living in Tokyo. I absolutely love using my creative side together in combination with logic and my problem solving skills in order to create beautiful looking functional apps.</p>
         <p>Since moving to Tokyo, I've been working as an English Language Instructor and then before that in Australia as an Operations Associate. I took a leap and enrolled in the Full Stack Web Dev course at Le Wagon and the rest is history!</p>
         <p>Naturally, most of my time is dedicated to coding but when I have a free moment I love to find new ramen spots that I haven't been to before around Tokyo (Jiro FTW!) as well as a bit of gaming here and there and finally Brazilian Jiu-jitsu to keep healthy!.</p>
+      </div>
+
+      <div className='stage-cube-cont'>
+        <div className='cubespinner'>
+          <div className='face1'>
+            <FontAwesomeIcon
+              icon={faReact}
+              color='#DD0031'
+            />
+          </div>
+          <div className='face2'>
+            <FontAwesomeIcon
+              icon={faHtml5}
+              color='#DD0031'
+            />
+          </div>
+          <div className='face3'>
+            <FontAwesomeIcon
+              icon={faCss3}
+              color='#DD0031'
+            />
+          </div>
+          <div className='face4'>
+            <FontAwesomeIcon
+              icon={faSmile}
+              color='#DD0031'
+            />
+          </div>
+          <div className='face5'>
+            <FontAwesomeIcon
+              icon={faJsSquare}
+              color='#DD0031'
+            />
+          </div>
+          <div className='face6'>
+            <FontAwesomeIcon
+              icon={faGitAlt}
+              color='#DD0031'
+            />
+          </div>
+        </div>
       </div>
     </div>
   )
