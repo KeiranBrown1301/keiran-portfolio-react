2d38f88ca05c70c4113d49cd69f21715fff66b82
Keiran Brown <keiranbrown1301@gmail.com>
2022-10-03 15:43:31 +0900

    added animated letters and useEffect, useState to contact page.

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,14 +1,39 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Loader from 'react-loaders'
 import Sidebar from '../Sidebar/Sidebar'
 import './contact.scss'
+import AnimatedLetters from '../AnimatedLetters/AnimatedLetters'
 
 function Contact() {
+  const [letterClass, setLetterClass] = useState('text-animate')
+
+  useEffect(() => {
+    setTimeout(() => {
+      return setLetterClass('text-animate-hover')
+    }, 3000)
+  }, [])
+
   return (
     <>
       <Sidebar />
-      <div>Contact Me</div>
-      <Loader type='pacman' />
+      <div className="container contact-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={['C', 'o', 'n', 't', 'a', 'c', 't', ' ', 'm', 'e']}
+              idx={15}
+            />
+          </h1>
+          <p>
+            I'm open to any job opportunities, from freelance to full time work.
+            If you have any questions or want to chat, please feel free to contact me
+            using the form below.
+          </p>
+          <div
+        </div>
+      </div>
+      <Loader type="pacman" />
     </>
   )
 }
